0a5ca0e16fad20740f3eb67c5cc8144ce6d52f1f
YangKian <1207783292@qq.com>
2021-12-30 13:33:18 +0800

    add basic test

--- app/src/test/java/io/hstream/testing/TestUtils.java
+++ app/src/test/java/io/hstream/testing/TestUtils.java
@@ -1,16 +1,17 @@
 package io.hstream.testing;
 
-import io.hstream.HStreamClient;
-import io.hstream.Subscription;
-import io.hstream.SubscriptionOffset;
+import io.hstream.*;
 import io.hstream.SubscriptionOffset.SpecialOffset;
 import java.io.File;
 import java.io.PrintWriter;
 import java.nio.file.Path;
-import java.util.UUID;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
@@ -28,33 +29,46 @@ public class TestUtils {
   }
 
   public static String randSubscriptionWithOffset(
-      HStreamClient c, String streamName, SpecialOffset offset) {
+      HStreamClient c, String streamName, SubscriptionOffset offset) {
     String subscriptionName = "test_subscription_" + randText();
     Subscription subscription =
         Subscription.newBuilder().subscription(subscriptionName).stream(streamName)
-            .offset(new SubscriptionOffset(offset))
+            .offset(offset)
             .ackTimeoutSeconds(10)
             .build();
     c.createSubscription(subscription);
     return subscriptionName;
   }
 
+  public static String randSubscriptionWithTimeout(
+      HStreamClient c, String streamName, int timeout) {
+    String subscriptionName = "test_subscription_" + randText();
+    Subscription subscription =
+        Subscription.newBuilder().subscription(subscriptionName).stream(streamName)
+            .offset(new SubscriptionOffset(SpecialOffset.EARLIEST))
+            .ackTimeoutSeconds(timeout)
+            .build();
+    c.createSubscription(subscription);
+    return subscriptionName;
+  }
+
   public static String randSubscription(HStreamClient c, String streamName) {
-    return randSubscriptionWithOffset(c, streamName, SpecialOffset.LATEST);
+    return randSubscriptionWithOffset(c, streamName, new SubscriptionOffset(SpecialOffset.LATEST));
   }
 
   public static String randSubscriptionFromEarliest(HStreamClient c, String streamName) {
-    return randSubscriptionWithOffset(c, streamName, SpecialOffset.EARLIEST);
+    return randSubscriptionWithOffset(
+        c, streamName, new SubscriptionOffset(SpecialOffset.EARLIEST));
   }
 
   // -----------------------------------------------------------------------------------------------
 
   public static GenericContainer<?> makeZooKeeper() {
-    return new GenericContainer(DockerImageName.parse("zookeeper")).withNetworkMode("host");
+    return new GenericContainer<>(DockerImageName.parse("zookeeper")).withNetworkMode("host");
   }
 
   public static GenericContainer<?> makeHStore(Path dataDir) {
-    return new GenericContainer(DockerImageName.parse("hstreamdb/hstream:latest"))
+    return new GenericContainer<>(DockerImageName.parse("hstreamdb/hstream:latest"))
         // .withNetwork(network)
         .withNetworkMode("host")
         .withFileSystemBind(
@@ -81,7 +95,7 @@ public class TestUtils {
       String zkHost,
       String hstoreHost,
       int serverId) {
-    return new GenericContainer(DockerImageName.parse("hstreamdb/hstream:v0.6.0"))
+    return new GenericContainer<>(DockerImageName.parse("hstreamdb/hstream:v0.6.0"))
         .withNetworkMode("host")
         .withFileSystemBind(dataDir.toAbsolutePath().toString(), "/data/hstore", BindMode.READ_ONLY)
         .withCommand(
@@ -106,7 +120,10 @@ public class TestUtils {
                 + " --store-admin-host "
                 + hstoreHost
                 + " --store-admin-port "
-                + "6440")
+                + "6440"
+                + " --log-level "
+                + "debug"
+                + " --log-with-color")
         .waitingFor(Wait.forLogMessage(".*Server started on port.*", 1));
   }
 
@@ -131,32 +148,12 @@ public class TestUtils {
 
   // -----------------------------------------------------------------------------------------------
 
-  public static Comparator<ReceivedRawRecord> ReceivedRawRecordComparator() {
-    return (r1, r2) -> {
-      var o1 = r1.getRecordId();
-      var o2 = r2.getRecordId();
-      if (o1.getBatchId() == o2.getBatchId()) {
-        return o1.getBatchIndex() - o2.getBatchIndex();
-      } else {
-        return (int) (o1.getBatchId() - o2.getBatchId());
-      }
-    };
-  }
-
-  public static boolean compareRecordIdAscending(RecordId a, RecordId b) {
-    if (a.getBatchId() == b.getBatchId()) {
-      return a.getBatchIndex() < b.getBatchIndex();
-    } else {
-      return a.getBatchId() < b.getBatchId();
-    }
-  }
-
   public static boolean isAscending(List<RecordId> input) {
     if (input.isEmpty()) return false;
     if (input.size() == 1) return true;
 
     for (int i = 1; i < input.size(); i++) {
-      if (!compareRecordIdAscending(input.get(i - 1), input.get(i))) {
+      if (input.get(i - 1).compareTo(input.get(i)) >= 0) {
         return false;
       }
     }
@@ -164,6 +161,100 @@ public class TestUtils {
   }
 
   public static void assertRecordIdsAscending(List<ReceivedRawRecord> input) {
-    Assertions.assertTrue(isAscending(input.stream().map(ReceivedRawRecord::getRecordId).toList()));
+    Assertions.assertTrue(
+        isAscending(
+            input.stream().map(ReceivedRawRecord::getRecordId).collect(Collectors.toList())));
+  }
+
+  public static Consumer createConsumer(
+      HStreamClient client,
+      String subscription,
+      String name,
+      List<ReceivedRawRecord> records,
+      CountDownLatch latch) {
+    return client
+        .newConsumer()
+        .subscription(subscription)
+        .name(name)
+        .rawRecordReceiver(
+            (receivedRawRecord, responder) -> {
+              records.add(receivedRawRecord);
+              responder.ack();
+              latch.countDown();
+            })
+        .build();
+  }
+
+  public static Consumer createConsumerWithFixNumsRecords(
+      HStreamClient client,
+      int nums,
+      String subscription,
+      String name,
+      List<ReceivedRawRecord> records,
+      CountDownLatch latch) {
+    final int maxReceivedCountC1 = nums;
+    AtomicInteger c1ReceivedRecordCount = new AtomicInteger(0);
+    return client
+        .newConsumer()
+        .subscription(subscription)
+        .name(name)
+        .rawRecordReceiver(
+            (receivedRawRecord, responder) -> {
+              if (c1ReceivedRecordCount.get() < maxReceivedCountC1) {
+                records.add(receivedRawRecord);
+                responder.ack();
+                if (c1ReceivedRecordCount.incrementAndGet() == maxReceivedCountC1) {
+                  latch.countDown();
+                }
+              }
+            })
+        .build();
+  }
+
+  public static Consumer createConsumerCollectStringPayload(
+      HStreamClient client,
+      String subscription,
+      String name,
+      List<String> records,
+      CountDownLatch latch) {
+    return client
+        .newConsumer()
+        .subscription(subscription)
+        .name(name)
+        .rawRecordReceiver(
+            (receivedRawRecord, responder) -> {
+              records.add(Arrays.toString(receivedRawRecord.getRawRecord()));
+              responder.ack();
+              latch.countDown();
+            })
+        .build();
+  }
+
+  public static ArrayList<String> doProduce(Producer producer, int payloadSize, int recordsNums) {
+    Random rand = new Random();
+    byte[] rRec = new byte[payloadSize];
+    var records = new ArrayList<String>();
+    var xs = new CompletableFuture[recordsNums];
+    for (int i = 0; i < recordsNums; i++) {
+      rand.nextBytes(rRec);
+      records.add(Arrays.toString(rRec));
+      xs[i] = producer.write(rRec);
+    }
+    CompletableFuture.allOf(xs).join();
+    return records;
+  }
+
+  public static ArrayList<RecordId> doProduceAndGatherRid(
+      Producer producer, int payloadSize, int recordsNums) {
+    var rids = new ArrayList<RecordId>();
+    Random rand = new Random();
+    byte[] rRec = new byte[payloadSize];
+    var writes = new ArrayList<CompletableFuture<RecordId>>();
+    for (int i = 0; i < recordsNums; i++) {
+      rand.nextBytes(rRec);
+      writes.add(producer.write(rRec));
+    }
+    writes.forEach(w -> w.thenAccept(rids::add));
+    return rids;
   }
 }
